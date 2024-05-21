aee9796c24d7c25f2c2c4973ea73a19683175bc6
guaingolotti <gusa@MacBook-Air-de-guada.local>
2024-05-21 11:25:45 -0300

    comienzo de practica integradora

--- practica-dom/js/ejercicio1.js
+++ practica-dom/js/ejercicio1.js
@@ -1,15 +1,23 @@
+//PRACTICA INTEGRADORA//
+
 let mensaje = "Bienvenidos a mi sitio."
 alert(mensaje);
 
-let seguro = "estas seguro de querer avanzar?"
+let seguro = "estas seguro de querer avanzar?";
 confirm(seguro);
-
-if(seguro){
-    document.querySelector("h2").innerText = 'que alegria que quieras continuar con tu visita';
-} else{
-    document.querySelector("h2").innerText = 'lamentamos que no quieras continuar con tu visita'
+if (seguro === true){
+   document.querySelector("h2").innerText = "que alegria que quieras continuar con tu visita";
+}else{
+   document.querySelector("h2").innerText = "lamentamos que no quieras seguir con tu visita";
 };
 
+let nombre = "ingrese su nombre";
+prompt(nombre);
 
-let nombre = "ingrese su nombre"
-prompt(nombre)
+document.querySelector("h1").innerText = "bienvenido" + ' ' + nombre;
+
+let edad = "cuantos años tenes?";
+prompt(edad);
+if(edad >= 17){
+  
+}
