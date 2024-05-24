d1364f3634990d9d49d98dc030903f940c7d7b1a
guaingolotti <gusa@Air-de-guada.fibertel.com.ar>
2024-05-23 23:40:01 -0300

    ultimas modificaciones de la practica DOM

--- practica-dom/js/ejercicio1.js
+++ practica-dom/js/ejercicio1.js
@@ -1,23 +1,48 @@
 //PRACTICA INTEGRADORA//
 
-let mensaje = "Bienvenidos a mi sitio."
-alert(mensaje);
+let mensaje = alert("Bienvenidos a mi sitio.");
 
-let seguro = "estas seguro de querer avanzar?";
-confirm(seguro);
-if (seguro === true){
-   document.querySelector("h2").innerText = "que alegria que quieras continuar con tu visita";
-}else{
-   document.querySelector("h2").innerText = "lamentamos que no quieras seguir con tu visita";
+let seguro = confirm("estas seguro de querer avanzar?");
+if (seguro){
+   document.querySelector("h2.saludo").innerText = "que alegria que quieras continuar con tu visita";
+} else{
+   document.querySelector("h2.saludo").innerText = "lamentamos que no quieras continuar con tu visita";
 };
 
-let nombre = "ingrese su nombre";
-prompt(nombre);
+let nombre = prompt("ingrese su nombre");
+console.log(nombre);
+document.querySelector("h1").innerText = `Bienvenido ${nombre}`;
 
-document.querySelector("h1").innerText = "bienvenido" + ' ' + nombre;
+let edad = prompt("cuantos años tenes?");
+if (edad >= 17){
+   document.querySelector(".container-general").style.display = 'block';
+   document.querySelector("#accesoDenegado").style.display = 'none';
 
-let edad = "cuantos años tenes?";
-prompt(edad);
-if(edad >= 17){
-  
-}
+      let progra = confirm("te gusta la programacion?");
+      let imagenProgra = document.querySelector(".background-img");
+
+      if (progra){
+      imagenProgra.innerHTML = '<img src= "../img/programmer.jpeg">';
+      } else{
+      imagenProgra.innerHTML = '<img src = "../img/gatito.jpeg">';
+      };
+
+      let ruta = prompt("ingrese url de imagen para su avatar");
+      let avatar = document.querySelector('.avatar');
+      avatar.src = ruta;
+
+         let peliculas = {
+            nombre: '',
+            director: '',
+            duracion: '',
+            actor: '',
+         };
+
+         peliculas.nombre = prompt('cual es tu pelicula favorita');
+         peliculas.director = prompt('quien es el director?');
+         peliculas.duracion = prompt('cuantos minutos dura la pelicula?');
+         peliculas.actor = prompt('ingrese el nombre del actor')
+} else if (edad < 17){
+   document.querySelector(".container-general").style.display = 'none';
+   document.querySelector("#accesoDenegado").style.display = 'block';
+};
