f35e5ce7a05e1f76e505122d7cda00170099457a
guaingolotti <gusa@Air-de-guada.fibertel.com.ar>
2024-05-25 14:05:16 -0300

    arreglos para practica DOM y parte de la practica de eventos terminada

--- practica-dom/js/ejercicio1.js
+++ practica-dom/js/ejercicio1.js
@@ -7,11 +7,16 @@ if (seguro){
    document.querySelector("h2.saludo").innerText = "que alegria que quieras continuar con tu visita";
 } else{
    document.querySelector("h2.saludo").innerText = "lamentamos que no quieras continuar con tu visita";
+   document.querySelector(".container-bienvenida").style.display = 'none';
 };
 
 let nombre = prompt("ingrese su nombre");
 console.log(nombre);
 document.querySelector("h1").innerText = `Bienvenido ${nombre}`;
+
+
+let container = document.querySelector(".container-general");
+let denegado = document.querySelector(".contenido-bloqueado");
 
 let edad = prompt("cuantos años tenes?");
 if (edad >= 17){
@@ -41,8 +46,15 @@ if (edad >= 17){
          peliculas.nombre = prompt('cual es tu pelicula favorita');
          peliculas.director = prompt('quien es el director?');
          peliculas.duracion = prompt('cuantos minutos dura la pelicula?');
-         peliculas.actor = prompt('ingrese el nombre del actor')
-} else if (edad < 17){
-   document.querySelector(".container-general").style.display = 'none';
-   document.querySelector("#accesoDenegado").style.display = 'block';
+         peliculas.actor = prompt('ingrese el nombre del actor');
+
+         document.querySelector("#nombre").innerText = peliculas.nombre;
+         document.querySelector("#director").innerText = peliculas.director;
+         document.querySelector("#duracion").innerText = peliculas.duracion + ' minutos';
+         document.querySelector("#actor").innerText = peliculas.actor;
+         
+         document.querySelector("#pelicula").style.display = 'block';
+} else {
+   document.querySelector("#accesoDenegado").style.display = 'flex';
+   container.style.display = 'none';
 };
---
+++ practica-eventos/js/main.js
@@ -0,0 +1,68 @@
+// completar las variables para que el programa funcione
+
+window.addEventListener('load', function() {
+
+    //capturar HTML 
+    let titulo = document.querySelector("#titulo");
+    titulo.addEventListener('mouseover', function(){
+    let nombre = prompt("cual es tu nombre?");
+    
+    console.log("El nombre es:", nombre)
+
+let saludo = document.querySelector("#saludo");
+let span = document.querySelector(".bienvenida span");
+if (nombre == false || nombre === ""){
+    saludo.innerText = 'Bienvenid@ usuario';
+    saludo.style.textTransform = "uppercase";
+    titulo.style.display = "none";
+    span.style.display = "block";
+} else {
+    document.querySelector("#saludo").innerText = 'bienvenido ' + nombre;
+    saludo.style.textTransform = "uppercase";
+    titulo.style.display = "none";
+    span.style.display = "block";
+}
+});
+
+
+span.addEventListener("click", function(){
+    let secbienvenida = document.querySelector(".bienvenida");
+    secbienvenida.style.display = "block";
+    let secpersonajes = document.querySelector(".personajes");
+    secpersonajes.style.display = "flex";
+});
+
+
+let bart = this.document.querySelector("#bart");
+let lisa = this.document.querySelector("#lisa");
+let marge = this.document.querySelector("#marge");
+let maggie = this.document.querySelectorAll("#maggie");
+let milhouse = this.document.querySelector("#milhouse");
+let burns = this.document.querySelector("#burns");
+let bobPatinio = this.document.querySelector("#bobPatinio");
+let flanders = this.document.querySelector("#flanders");
+let duffman =  this.document.querySelector("#duffman");
+let gregory = this.document.querySelector("#gregory");
+let nelson = this.document.querySelector("#nelson");
+
+let bartDos = this.document.querySelector("#bart span");
+let lisaDos = this.document.querySelector("#lisa span");
+let margeDos = this.document.querySelector("#marge span");
+let maggieDos = this.document.querySelectorAll("#maggie span");
+let milhouseDos = this.document.querySelector("#milhouse span");
+let burnsDos = this.document.querySelector("#burns span");
+let bobPatinioDos = this.document.querySelector("#bobPatinio span");
+let flandersDos = this.document.querySelector("#flanders span");
+let duffmanDos =  this.document.querySelector("#duffman span");
+let gregoryDos = this.document.querySelector("#gregory span");
+let nelsonDos = this.document.querySelector("#nelson span");
+ 
+    /* Capturar pesonajes + Asignarles a cada uno su evento correspondiente, 
+    así como también su funcionalidad */
+    
+
+    /* Asignarle evento a la variable boton */
+    //let boton = ;
+
+ 
+ }) 
